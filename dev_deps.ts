d768eae5c2b5c2682793929ad9c5b04350db04a1
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-02-10 20:41:00 +0000

    setup test harness

---
+++ dev_deps.ts
@@ -0,0 +1,8 @@
+export { test, runIfMain } from "https://deno.land/std/testing/mod.ts";
+export {
+  assertEquals,
+  assertNotEquals,
+  assertThrowsAsync,
+  assertThrows,
+  assert
+} from "https://deno.land/std/testing/asserts.ts";
---
+++ lib/dake_test.ts
@@ -0,0 +1,7 @@
+import { test, assertEquals } from "../dev_deps.ts";
+import { Dake } from "./dake.ts";
+
+test(function testHello() {
+  const enc = new TextEncoder();
+  // Deno.writeFile()
+});
